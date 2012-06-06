2910a3ae9b530add0957cfcd3e58b37c27ca3f21
Mark Hayes <mark@deployfx.com>
2012-06-05 21:40:46 -0700

    comment out node attributes file

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 cookbook_path File.expand_path("../cookbooks", __FILE__)
-json_attribs File.expand_path("../node.json", __FILE__)
-role_path File.expand_path("../roles", __FILE__)
+#json_attribs File.expand_path("../node.json", __FILE__)
+#role_path File.expand_path("../roles", __FILE__)
